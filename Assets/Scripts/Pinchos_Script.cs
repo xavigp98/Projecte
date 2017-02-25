9a9f33f327b170cabb92d2e8ccfdf09801bf9a26
REgiSTerKZz <joanreigbergua@enti.cat>
2017-02-25 13:40:28 +0100

    Projecte 1
    Nou projecte amb els noms ben posats i nome suna escena anomenada
    prototip

---
+++ Assets/Scripts/Pinchos_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pinchos_Script : MonoBehaviour {
+    private int health = 10;
+    public bool isDamaged = false;
+    IEnumerator OnTriggerEnter2D(Collider2D collision)
+    {       
+        isDamaged = true;
+        
+        if(isDamaged == true && collision.gameObject.tag == "enemy")
+        {
+            health = health - 1;
+            yield return new WaitForSeconds (3);
+            isDamaged = false;
+        }
+      
+
+
+    }
+}
