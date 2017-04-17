99fc3d094639cbba2a4c21ec305185112c32adfe
xavigp98 <xaviergraciapereniguez@enti.cat>
2017-04-18 00:02:23 +0200

    Forzada la resolucion a 1080 ( ya veremos que hacemos con esto)

---
+++ Assets/Scripts/Resolucion.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resolucion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Screen.SetResolution(1920, 1080, true);
+	}
+}
